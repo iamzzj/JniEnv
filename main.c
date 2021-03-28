e46be7266e1888b5381dd3d2d4b72592603240dc
iamzzj <iamzzj@qq.com>
2021-03-28 15:04:05 +0800

    c调Java

--- main.c
+++ main.c
@@ -26,5 +26,30 @@ int main() {
 
     printf("%s", jstring);
 
+    /*//获取jclass
+    jclass j_class = env->GetObjectClass(j_object);
+
+    //GetFieldID(jclass clazz, const char* name, const char* sig)
+    //sig属性的签名
+    jfieldID j_fieldId = env->GetFieldID(j_class, "name", "Ljava/lang/String");
+    env->GetStaticFieldID //静态
+
+    //获取name属性的fild
+    jstring
+    j_string = (jstring)
+    env->GetObjectField(j_object, j_fieldId);
+    env->GetStaticObjectField(j_object, j_fieldId);//静态
+
+     //获取方法
+     env->CallVoidMethod()
+
+    //打印字符串 jstring -> c str
+    const char *c_str = env->GetStringUTFChars(j_string, NULL);
+    printf("name: %s", c_str);
+
+    //修改成jack
+    jstring jackName = env->NewStringUTF("Jack");
+    env->SetObjectField(j_object, j_fieldId, jackName);*/
+
     return 0;
 }
